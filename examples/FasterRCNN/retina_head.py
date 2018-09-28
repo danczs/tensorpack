a94353f40041a1ccbba879d7d9c981e6f29f133e
danczs <danczs@buaa.edu.cn>
2018-09-28 15:56:16 +0800

    Update retina_head.py

--- examples/FasterRCNN/retina_head.py
+++ examples/FasterRCNN/retina_head.py
@@ -1,1 +1,27 @@
 #retinal net head
+def decode_bbox_target(box_predictions, anchors):
+    """
+    Args:
+        box_predictions: (..., 4), logits
+        anchors: (..., 4), floatbox. Must have the same shape
+
+    Returns:
+        box_decoded: (..., 4), float32. With the same shape.
+    """
+    orig_shape = tf.shape(anchors)
+    box_pred_txtytwth = tf.reshape(box_predictions, (-1, 2, 2))
+    box_pred_txty, box_pred_twth = tf.split(box_pred_txtytwth, 2, axis=1)
+    # each is (...)x1x2
+    anchors_x1y1x2y2 = tf.reshape(anchors, (-1, 2, 2))
+    anchors_x1y1, anchors_x2y2 = tf.split(anchors_x1y1x2y2, 2, axis=1)
+
+    waha = anchors_x2y2 - anchors_x1y1
+    xaya = (anchors_x2y2 + anchors_x1y1) * 0.5
+
+    clip = np.log(config.PREPROC.MAX_SIZE / 16.)
+    wbhb = tf.exp(tf.minimum(box_pred_twth, clip)) * waha
+    xbyb = box_pred_txty * waha + xaya
+    x1y1 = xbyb - wbhb * 0.5
+    x2y2 = xbyb + wbhb * 0.5    # (...)x1x2
+    out = tf.concat([x1y1, x2y2], axis=-2)
+    return tf.reshape(out, orig_shape)
